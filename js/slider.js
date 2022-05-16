8ee1506db1363ff72df4dc5b95abec377d6bbc38
samgxlden <samgxlden@gmail.com>
2022-05-16 23:17:31 +0300

    create-base-template-v_1.5

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,20 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 })
+
+let item1 = document.querySelector(".hero__slider_item1")
+let item2 = document.querySelector(".hero__slider_item2")
+const item1_img = document.querySelector(".hero__slider_item1_img")
+const item2_img = document.querySelector(".hero__slider_item2_img")
+
+item1_img.addEventListener("click", function () {
+  item2.classList.add("_active")
+  item2.classList.remove("_disabled")
+  item1.classList.add("_disabled")
+})
+
+item2_img.addEventListener("click", function () {
+  item1.classList.add("_active")
+  item1.classList.remove("_disabled")
+  item2.classList.add("_disabled")
+})
