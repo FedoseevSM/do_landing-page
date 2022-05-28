4f478b418d833dfbc6a1c54978396cded8b5dcc9
samgxlden <samgxlden@gmail.com>
2022-05-28 23:16:00 +0300

    create-base-template-v_1.6

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - 15.05 - Добавлена форма подписки
 - 15.05 - Добавил слайдер и обновил футер
 - 16.05 - Исправил верхнее меню и добавил второй слайдер
+- 28.05 - Добавил адаптивное отображение
 
 - __GitHub Pages:__ https://samgxlden.github.io/do_landing-page/
 - __Figma__: https://www.figma.com/file/CRHuH8WbX2m7y9pZipiuXb/digitalagencyuplabs-(Copy)?node-id=0%3A1
--- js/slider.js
+++ js/slider.js
@@ -8,10 +8,14 @@ const swiper = new Swiper(".swiper", {
   },
 })
 
-let item1 = document.querySelector(".hero__slider_item1")
-let item2 = document.querySelector(".hero__slider_item2")
+const item1 = document.querySelector(".hero__slider_item1")
+const item2 = document.querySelector(".hero__slider_item2")
 const item1_img = document.querySelector(".hero__slider_item1_img")
 const item2_img = document.querySelector(".hero__slider_item2_img")
+const burger = document.querySelector(".burger")
+const headerMenu = document.querySelector(".header__menu")
+const main = document.querySelector(".main")
+const footer = document.querySelector(".footer")
 
 item1_img.addEventListener("click", function () {
   item2.classList.add("_active")
@@ -23,4 +27,24 @@ item2_img.addEventListener("click", function () {
   item1.classList.add("_active")
   item1.classList.remove("_disabled")
   item2.classList.add("_disabled")
+})
+
+function activeHeader(isActive) {
+  // Функия которая определяет если шапка активна и делает манипуляций в ней
+  if (isActive) {
+    main.classList.add("_white-bg")
+    footer.classList.add("_white-bg")
+    document.getElementById("burger").src = "./img/menu-2.png"
+    headerMenu.classList.add("_active-burger")
+  } else {
+    main.classList.remove("_white-bg")
+    footer.classList.remove("_white-bg")
+    document.getElementById("burger").src = "./img/menu-1.png"
+    headerMenu.classList.remove("_active-burger")
+  }
+}
+activeHeader(burger.classList.contains("_active"))
+burger.addEventListener("click", function () {
+  burger.classList.toggle("_active")
+  activeHeader(burger.classList.contains("_active"))
 })
